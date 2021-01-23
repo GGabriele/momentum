411ec9f57a78d30d1bb29d511f0cb43d630a7e89
Gabriele Gerbino <gabrielegerbino@gmail.com>
2021-01-23 18:06:25 +0100

    Add exclude_symbols

--- momentum/collect.py
+++ momentum/collect.py
@@ -15,7 +15,8 @@ from .utils import collect_history, get_sp500_symbols
 )
 def collect(ctx, start, end, **kwargs):
     """Collect and store historical SP500 data."""
-    symbols = get_sp500_symbols()
+    exclude = []
+    symbols = get_sp500_symbols(exclude)
     # Add SPY ticker to the symbols to include it into history.
     symbols.append("SPY")
     start = datetime.strptime(start, "%Y-%m-%d")
